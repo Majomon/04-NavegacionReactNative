e277b1a5c00069153c27093f5c1e69286c7aa037
Mauri <mauri.monzon91@gmail.com>
2023-08-24 17:01:55 -0300

    Problemas con drawe navegacion

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {StackNavigator} from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
+/* import {StackNavigator} from './src/navigator/StackNavigator'; */
+
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/*       <StackNavigator /> */}
+      <MenuLateralBasico/>
     </NavigationContainer>
   );
 };
