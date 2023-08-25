eee839cea9d4482f7bec7fd57a1638e8a289cda7
Mauri <mauri.monzon91@gmail.com>
2023-08-24 23:30:04 -0300

    Terminado navegacion con stack

--- src/screens/PersonaScreen.tsx
+++ src/screens/PersonaScreen.tsx
@@ -2,15 +2,26 @@ import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {StackScreenProps} from '@react-navigation/stack';
+import {RootStackParams} from '../navigator/StackNavigator';
 
-interface Props extends StackScreenProps<any, any> {}
+/* Alternativa */
+/* interface RouteParams {
+  id: number;
+  name: string;
+} */
+
+// Primero el type"StackScreenProps" , luego nombre de la ruta "PersonaScreen"
+interface Props extends StackScreenProps<RootStackParams, 'PersonaScreen'> {}
 
 export const PersonaScreen = ({navigation, route}: Props) => {
+  /* Alternativa */
+  /*   const params = route.params as RouteParams; */
+
   const params = route.params;
 
   useEffect(() => {
     navigation.setOptions({
-      title: params!.name as any,
+      title: params.name,
     });
   }, [navigation, params]);
 
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -4,13 +4,26 @@ export const styles = StyleSheet.create({
   globalMargin: {
     marginHorizontal: 20,
   },
+  containerPersona: {
+    flexDirection: 'row',
+  },
   title: {
     fontSize: 25,
     color: 'black',
     marginBottom: 10,
   },
-  textoPersonas: {
+  botonGrande: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  botonGrandeTexto: {
     fontSize: 20,
-    color: 'red',
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
