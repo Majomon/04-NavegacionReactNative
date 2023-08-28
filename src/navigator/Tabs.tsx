6b85e52d6ea77c4d364c8a97414c17849f9c6bef
Mauri <mauri.monzon91@gmail.com>
2023-08-27 23:47:47 -0300

    Material bottom tabs

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,21 +1,69 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Tab1Screen from '../screens/Tab1Screen';
 import Tab2Screen from '../screens/Tab2Screen';
-import {colores} from '../theme/appTheme';
 import {StackNavigator} from './StackNavigator';
+import {colores} from '../theme/appTheme';
 import {Text} from 'react-native';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export const Tabs = () => {
   return (
     <Tab.Navigator
-      /* Toda la escena/pagina */
+      sceneAnimationEnabled={true}
+      barStyle={{backgroundColor: colores.primary}}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color}) => {
+          let iconName: string = '';
+
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+            case 'StackNavigator':
+              iconName = 'ST';
+              break;
+            default:
+              return;
+          }
+
+          return <Text style={{color}}>{iconName}</Text>;
+        },
+      })}>
+      <Tab.Screen
+        name="Tab1Screen"
+        options={{title: 'Tab1'}}
+        component={Tab1Screen}
+      />
+      <Tab.Screen
+        name="Tab2Screen"
+        options={{title: 'Tab2'}}
+        component={Tab2Screen}
+      />
+      <Tab.Screen
+        name="StackNavigator"
+        options={{title: 'Stack'}}
+        component={StackNavigator}
+      />
+    </Tab.Navigator>
+  );
+};
+
+// PAra IOS
+/* const Tab = createBottomTabNavigator();
+
+export const Tabs = () => {
+  return (
+    <Tab.Navigator
+      // Toda la escena/pagina
       sceneContainerStyle={{
         backgroundColor: 'violet',
       }}
-      /* Propio de los tabs */
+      // Propio de los tabs
       screenOptions={({route}) => ({
         tabBarActiveTintColor: colores.primary,
         tabBarStyle: {
@@ -48,7 +96,7 @@ export const Tabs = () => {
         options={{
           title: 'Tab 1',
           // Para tenerlo como referencia
-          /*        tabBarIcon: props => <Text style={{color: props.color}}>T1</Text>, */
+          // tabBarIcon: props => <Text style={{color: props.color}}>T1</Text>
         }}
         component={Tab1Screen}
       />
@@ -64,4 +112,4 @@ export const Tabs = () => {
       />
     </Tab.Navigator>
   );
-};
+}; */
