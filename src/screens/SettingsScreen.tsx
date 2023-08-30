45d4c859929bed84d2a6aedf3954bacc47f0ab88
Mauri <mauri.monzon91@gmail.com>
2023-08-30 08:50:13 -0300

    Mostrando los estados en settings

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Text, View} from 'react-native';
 import {styles} from '../theme/appTheme';
+import {AuthContext} from '../context/AuthContext';
 
 export const SettingsScreen = () => {
+  const {authState} = useContext(AuthContext);
+
   return (
     <View style={styles.globalMargin}>
       <Text style={styles.title}>Settings Screen</Text>
+      <Text style={styles.title}>{JSON.stringify(authState, null, 4)}</Text>
     </View>
   );
 };
